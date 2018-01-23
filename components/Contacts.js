642284642ff9626199056956308b952ff6f7daf8
Pawel Furman <pf.furman@gmail.com>
2018-01-23 20:51:41 +0100

    Remove unnecessary console.log

--- components/Contacts.js
+++ components/Contacts.js
@@ -3,14 +3,11 @@ Contacts = React.createClass({
     items: React.PropTypes.array.isRequired,
   },
 
-
   render: function() {
     var contacts = this.props.items.map(function(contact) {
         return (<Contact key={contact.id} item={contact}>Item {contact.id}</Contact>)
     });
-
-    console.log(contacts);
-
+    
     return (
       <div className={'contactsList'}>
         {contacts}
